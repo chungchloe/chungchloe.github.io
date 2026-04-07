fbfcc3dae17f144a23afbdab134dc98eb636ce29
chungchloe <chungchloe@pm.me>
2026-04-07 02:58:33 -0400

    fix vite deps and add pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: process.env.VITE_BASE || '/',
+  //base: process.env.VITE_BASE || '/',
   //base: '/chungchloe.github.io',
 });
